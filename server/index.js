ba9fb18c50c85d3552e8b2c6df9655addc0f00fe
Jérôme Blondon <jerome@blondon.fr>
2020-10-01 18:49:15 +0200

    Add html preview

--- server/index.js
+++ server/index.js
@@ -4,6 +4,37 @@ const bodyParser = require("body-parser");
 const logger = require("morgan");
 const extractor = require("../extractor");
 
+function toTemplate(data) {
+  const template = `
+  <h1>${data.article.title}</h1>
+  <img src="${data.screenshot.thumbnail}" >
+
+  <h2>Excerpt</h2>
+  <div class="article">
+  ${data.article.excerpt}
+  </div>
+
+  <h2>Keywords</h2>
+  ${JSON.stringify(data.keywords)}
+
+  <h2>Colors</h2>
+  <div style="display:inline-block; width:64px; height: 64px; margin: 24px; background-color: ${data.screenshot.colors.color};"></div>
+  <div style="display:inline-block; width:64px; height: 64px; margin: 24px; background-color: ${data.screenshot.colors.palette[0]};" /></div>
+  <div style="display:inline-block; width:64px; height: 64px; margin: 24px; background-color: ${data.screenshot.colors.palette[1]};" /></div>
+  <div style="display:inline-block; width:64px; height: 64px; margin: 24px; background-color: ${data.screenshot.colors.palette[2]};" /></div>
+  <div style="display:inline-block; width:64px; height: 64px; margin: 24px; background-color: ${data.screenshot.colors.palette[3]};" /></div>
+  <div style="display:inline-block; width:64px; height: 64px; margin: 24px; background-color: ${data.screenshot.colors.palette[4]};" /></div>
+
+
+  <h2>Screenshots</h2>
+  <h3>Page</h3>
+  <img src="${data.screenshot.screenshot}" >
+  <h3>Article</h3>
+  ${data.article.content}
+  `
+  return template;
+};
+
 exports.start = function (port, hostname) {
   app.use(logger('dev'));
   app.use(bodyParser.urlencoded({ extended: true }));
@@ -13,7 +44,11 @@ exports.start = function (port, hostname) {
     const query = req.query;
     if (query.url) {
       extractor.extract(query.url).then((output) => {
-        res.send(output);
+        if (query.html) {
+          res.send(toTemplate(output))
+        } else {
+          res.send(output);
+        }
       }).catch((error) => {
         console.log(error)
         res.sendStatus(500);
